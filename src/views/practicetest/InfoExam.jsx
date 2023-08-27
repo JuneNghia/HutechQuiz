945364231cd1d8db5bccfccbc4c2de8912a7d66d
June Nghia <nguyenminhtrungnghia@gmail.com>
2023-08-27 16:11:42 +0700

    update

--- src/views/practicetest/InfoExam.jsx
+++ src/views/practicetest/InfoExam.jsx
@@ -1,5 +1,6 @@
 import { Box, Button, Card, CardContent, CardHeader, Typography } from '@mui/material'
 import React from 'react'
+import { formattedValuePrice } from '../../utils/common/formatValue'
 
 const InfoExam = ({ title, handleSubmit, quantity, price }) => {
   return (
@@ -8,7 +9,9 @@ const InfoExam = ({ title, handleSubmit, quantity, price }) => {
       <CardContent>
         <Box className='mt-2'>
           <Typography>Số câu : {quantity} câu</Typography>
-          <Typography>Số tiền : <span className='font-bold'>{price || '0'}đ</span> / lần</Typography>
+          <Typography>
+            Số tiền : <span className='font-bold'>{formattedValuePrice(price.toString()) || '0'}đ</span> / lần
+          </Typography>
         </Box>
 
         <Button sx={{ mt: 3 }} variant='contained' onClick={handleSubmit}>
