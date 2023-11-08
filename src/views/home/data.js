78bc42953e430d62247b7d4adffe2f1743fe2d36
June Nghia <nguyenminhtrungnghia@gmail.com>
2023-11-08 11:16:18 +0700

    update code

--- src/views/home/data.js
+++ src/views/home/data.js
@@ -1,17 +1,24 @@
 const dataNotify = [
   {
+    title: 'Bảo trì hệ thống ngày 11/11/2023',
+    html: 'Hiện nay, số lượng tài khoản người dùng cũng như lượng câu hỏi các môn ôn tập trên Hutech Quiz ngày càng nhiều. Do đó, để tối ưu hoá hệ thống, mở rộng tính năng và nâng cao chất lượng người dùng. Từ <i>0h-0h30 ngày 11/11/2023</i>, hệ thống sẽ thực hiện bảo trì để cải tiến dịch vụ và tất cả tài khoản có : <ul><li>- Thông tin <span class="text-red-500 font-bold">SỐ TIỀN ĐÃ NẠP</span> trong chức năng Ví Tiền <span class="text-red-500 font-bold">bằng 0</span> </li><li>- Tài khoản đã được tạo từ ngày <span class="text-red-500 font-bold">27/08/2023 đến 1/11/2023</span></li></ul>Sẽ bị <span class="text-red-500 font-bold">XOÁ VĨNH VIỄN</span> khỏi hệ thống',
+    color: 'error',
+    link: null,
+    label: 'THÔNG BÁO'
+  },
+  {
     title: 'Anh Ngữ / Tiếng Anh',
     html: 'Hiện tại Hutech Quiz đã cập nhập các môn học như Anh Ngữ / Tiếng Anh theo hình thức sách mà các bạn đang học.<br/><br/>Ví dụ, bạn học <b class="text-red-600">Anh Ngữ 1</b> và sử dụng giáo trình <b class="text-red-600">Sách 3A</b>, thì sẽ thi thử bằng cách nhấn vào <b class="text-blue-600">Anh Ngữ / Tiếng Anh → chọn sách 3A</b> để thi thử.',
     color: 'error',
     link: null,
-    label: 'Mới'
+    label: 'THÔNG BÁO'
   },
   {
     title: 'Kiểm tra Ví tiền',
     text: 'Di chuyển nhanh tới trang Ví tiền tại',
     color: 'warning',
     link: 'wallet',
-    label: 'Phím tắt'
+    label: 'PHÍM TẮT'
   }
 ]
 
