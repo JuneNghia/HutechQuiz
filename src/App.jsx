4281ac089a8923cb84afd3a83f9dba93b6b54689
June Nghia <nguyenminhtrungnghia@gmail.com>
2024-01-06 18:01:31 +0700

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ function App() {
   return (
     <Suspense fallback={<RouteLoader />}>
       <JWTProvider>
-        <div>
+        {/* <div>
           Do số lượng người dùng sử dụng hệ thống quá nhiều nên hệ thống sẽ bảo trì nâng cấp hệ thống sang máy chủ tốt
           hơn để phục vụ nhu cầu của các bạn trong mùa thi. <br />
           Thời gian dự kiến: 15:24 06/1/2024 đến 18:30 06/1/2024
           <br />
           HutechQuiz rất xin lỗi về sự cố này, mong các bạn thông cảm :(
-        </div>
-        {/* <RouterProvider router={router} /> */}
+        </div> */}
+        <RouterProvider router={router} />
       </JWTProvider>
     </Suspense>
   )
