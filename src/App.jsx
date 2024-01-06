226075af1e3e5fded37eabae69f5cf529398e4c6
June Nghia <nguyenminhtrungnghia@gmail.com>
2024-01-06 15:28:51 +0700

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { Suspense } from "react";
-import { JWTProvider } from "./contexts/JWTContext";
-import { RouterProvider } from "react-router-dom";
-import router from "./routes";
-import RouteLoader from "./components/Loader/RouteLoader";
+import { Suspense } from 'react'
+import { JWTProvider } from './contexts/JWTContext'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes'
+import RouteLoader from './components/Loader/RouteLoader'
 
 function App() {
   return (
     <Suspense fallback={<RouteLoader />}>
       <JWTProvider>
-        <RouterProvider router={router} />
+        <div>
+          Do số lượng người dùng sử dụng hệ thống quá nhiều nên hệ thống sẽ bảo trì nâng cấp hệ thống sang máy chủ tốt
+          hơn để phục vụ nhu cầu của các bạn trong mùa thi. <br />
+          Thời gian dự kiến: 15:24 06/1/2024 đến 18:30 06/1/2024
+          <br />
+          HutechQuiz rất xin lỗi về sự cố này, mong các bạn thông cảm :(
+        </div>
+        {/* <RouterProvider router={router} /> */}
       </JWTProvider>
     </Suspense>
-  );
+  )
 }
 
-export default App;
+export default App
