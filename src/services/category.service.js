b9cda79d12be25983839e388b26592f0d1c6d27b
June Nghia <nguyenminhtrungnghia@gmail.com>
2024-01-07 16:15:38 +0700

    update code

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -2,12 +2,14 @@ import axiosConfig from '../utils/axios'
 
 const CATEGORY = {
   GET_QUESTION_BY_ID: (id) => `/category/${id}/get-all-test`,
-  ADD_TEST: (id) => `/category/${id}/add-test`
+  ADD_TEST: (id) => `/category/${id}/add-test`,
+  EDIT_TEST: (id) => `/category/${id}/edit-test`
 }
 
 const CategoryService = {
   getQuesById: (id) => axiosConfig.get(CATEGORY.GET_QUESTION_BY_ID(id)),
-  addTest: (id, data) => axiosConfig.post(CATEGORY.ADD_TEST(id), data)
+  addTest: (id, data) => axiosConfig.post(CATEGORY.ADD_TEST(id), data),
+  editTest: (id, data) => axiosConfig.put(CATEGORY.EDIT_TEST(id), data)
 }
 
 export default CategoryService
--- src/views/admin/questionmanagement/index.jsx
+++ src/views/admin/questionmanagement/index.jsx
@@ -18,16 +18,24 @@ import { Helmet } from 'react-helmet'
 import SubjectService from '../../../services/subject.service'
 import CategoryService from '../../../services/category.service'
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined'
+import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined'
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined'
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined'
 import InputEditor from '../../../components/Input/ReactQuill'
 import StepTitle from '../../../components/StepTitle'
+import Swal from 'sweetalert2'
+import Radio from '@mui/material/Radio'
+import Tooltip from '@mui/material/Tooltip'
 
 const QuesManage = () => {
   const { user } = useAuth()
   const [listSubject, setListSubject] = useState([])
   const [selectedSubjectID, setSelectedSubjectID] = useState('')
+  const [selectedSubjectName, setSelectedSubjectName] = useState('')
   const [selectedCategoryID, setSelectedCategoryID] = useState()
   const [listCategory, setListCategory] = useState()
   const [isFetching, setIsFetching] = useState(false)
+  const [listEditQuestion, setListEditQuestion] = useState([])
 
   const [data, setData] = useState()
 
@@ -40,19 +48,140 @@ const QuesManage = () => {
 
     if (selectedSubjectID) {
       setListCategory(selectedSubjectID.category)
+      setSelectedSubjectName(selectedSubjectID.title)
     } else {
       setListCategory([])
     }
   }
+
+  console.log(selectedSubjectName)
 
   const handleCategoryChange = (event) => {
     const selected = event.target.value
     setSelectedCategoryID(selected)
   }
 
-  const handleQuestionChange = (id, newQuestion) => {}
+  const handleQuestionChange = (quesId, newQuestion) => {
+    setData((prev) => {
+      return prev.map((item) => {
+        if (item.id === quesId) {
+          return { ...item, question: newQuestion }
+        }
+        return item
+      })
+    })
+  }
 
-  const handleAnswerChange = (id, newAnswer) => {}
+  const handleAnswerChange = (quesId, newAnswer) => {
+    setData((prev) => {
+      return prev.map((item) => {
+        if (item.id === quesId) {
+          return { ...item, answer: newAnswer }
+        }
+        return item
+      })
+    })
+  }
+
+  const handleChoiceChange = (quesId, choiceIndex, newChoice) => {
+    setData((prev) => {
+      return prev.map((item) => {
+        if (item.id === quesId) {
+          return {
+            ...item,
+            choices: item.choices.map((choice, index) => (index === choiceIndex ? newChoice : choice))
+          }
+        }
+        return item
+      })
+    })
+  }
+
+  const handleEditQuestion = (quesId) => {
+    if (listEditQuestion.length === 5) {
+      Swal.fire(
+        'Lỗi',
+        'Bạn chỉ có thể sửa cùng lúc tối đa 5 câu hỏi, vui lòng nhấn nút hoàn thành các câu hỏi đã sửa trước. <br/><b>Mục đích những máy nào dởm dởm thì đỡ lag</b>',
+        'error'
+      )
+    } else {
+      setListEditQuestion((prev) => {
+        const newData = [...prev]
+        newData.push(quesId)
+        return newData
+      })
+    }
+  }
+
+  const handleFinishQuestion = (quesId) => {
+    setListEditQuestion((prev) => {
+      const newData = prev.filter((item) => item !== quesId)
+      return newData
+    })
+  }
+
+  const handleDeleteQuestion = (quesId) => {
+    if (data.length === 1) {
+      Swal.fire('Djt Me', 'Xoá hết câu hỏi thì lấy gì mà lưu ???', 'error')
+    } else {
+      setData((prev) => {
+        const newData = prev.filter((item) => item.id !== quesId)
+        return newData
+      })
+    }
+  }
+
+  const handleSubmit = () => {
+    const dataTest = data.map((ques) => ({
+      id: ques.id,
+      question: ques.question,
+      answer: ques.answer,
+      choices: ques.choices
+    }))
+
+    Swal.fire({
+      icon: 'question',
+      html: `Xác nhận lưu các chỉnh sửa câu hỏi cho môn học <br/><b>${selectedSubjectName}</b>`,
+      showCancelButton: true,
+      cancelButtonText: 'Huỷ',
+      confirmButtonText: 'Thêm',
+      preConfirm: () => {
+        Swal.update({
+          showCancelButton: false,
+          showConfirmButton: false,
+          icon: 'info',
+          html: 'Đang xử lý...<br/> Vui lòng không tắt trang hoặc trình duyệt'
+        })
+
+        CategoryService.editTest(selectedCategoryID, dataTest)
+          .then(() => {
+            setTimeout(() => {
+              Swal.update({
+                title: 'Xin Chúc Mừng',
+                icon: 'success',
+                html: `Cập nhật dữ liệu môn học <b>${selectedSubjectName}</b> thành công`,
+                showConfirmButton: false,
+                showCancelButton: true,
+                cancelButtonText: 'Xác nhận',
+                cancelButtonColor: 'green'
+              })
+            }, 1000)
+          })
+          .catch(() => {
+            Swal.update({
+              icon: 'error',
+              html: `Cập nhật câu hỏi thất bại`,
+              showConfirmButton: false,
+              showCancelButton: true,
+              cancelButtonText: 'Xác nhận',
+              cancelButtonColor: '#0000cd'
+            })
+          })
+
+        return false
+      }
+    })
+  }
 
   useEffect(() => {
     SubjectService.getAllSubject().then((res) => {
@@ -86,7 +215,7 @@ const QuesManage = () => {
           <Helmet>
             <title>Quản lý câu hỏi</title>
           </Helmet>
-          <StepTitle title={'Quản lý câu hỏi'}/>
+          <StepTitle title={'Quản lý câu hỏi'} />
           <Paper className='mb-4 p-4'>
             <FormGroup>
               <FormControl margin='normal'>
@@ -153,76 +282,103 @@ const QuesManage = () => {
                       title={
                         <>
                           <div className='flex justify-end mb-3'>
-                            <Button
-                              color='error'
-                              variant='contained'
-                              size='small'
-                              disabled
-                              // onClick={() => handleDeleteQuestion(quesIndex)}
-                            >
-                              <DeleteOutlineOutlinedIcon className='mr-2' /> Xoá câu hỏi
-                            </Button>
+                            {!listEditQuestion.find((item) => item === dataQues.id) && (
+                              <div className='flex gap-4'>
+                                <Button
+                                  onClick={() => handleEditQuestion(dataQues.id)}
+                                  color='warning'
+                                  size='small'
+                                  variant='contained'
+                                >
+                                  <EditOutlinedIcon className='mr-2' />
+                                  Sửa câu hỏi
+                                </Button>
+                                <Button
+                                  color='error'
+                                  variant='contained'
+                                  size='small'
+                                  onClick={() => handleDeleteQuestion(dataQues.id)}
+                                >
+                                  <DeleteOutlineOutlinedIcon className='mr-2' /> Xoá câu hỏi
+                                </Button>
+                              </div>
+                            )}
+
+                            {listEditQuestion.find((item) => item === dataQues.id) && (
+                              <Button
+                                color='success'
+                                variant='contained'
+                                size='small'
+                                onClick={() => handleFinishQuestion(dataQues.id)}
+                              >
+                                <CheckCircleOutlineOutlinedIcon className='mr-2' />
+                                Hoàn thành
+                              </Button>
+                            )}
                           </div>
                         </>
                       }
                     />
                     <CardContent>
                       <div className='flex items-center mb-3'>
-                        <div className='md:w-[8%] xs:w-[10%] font-bold'>CÂU {quesIndex + 1}: </div>
-                      <p dangerouslySetInnerHTML={{__html: dataQues.question}}/>
-                        {/* <InputEditor
-                          value={dataQues.question}
-                          handleChange={(e) => handleQuestionChange(dataQues.id, e)}
-                        /> */}
-                      </div>
-                      <div className='flex items-center'>
-                        <div className='md:w-[8%] xs:w-[10%]  font-bold'>Đáp án: </div>{' '}
-                        <p dangerouslySetInnerHTML={{__html: dataQues.answer}}/>
-                        {/* <InputEditor value={dataQues.answer} handleChange={(e) => handleAnswerChange(dataQues.id, e)}/> */}
+                        <div className='md:w-[6%] xs:w-[12%] font-bold'>CÂU {quesIndex + 1}: </div>
+
+                        {listEditQuestion.find((item) => item === dataQues.id) ? (
+                          <>
+                            <div className='w-[45px] h-[43px]'></div>
+                            <InputEditor
+                              placeholder={'Chưa có nội dung'}
+                              value={dataQues.question}
+                              handleChange={(e) => handleQuestionChange(dataQues.id, e)}
+                            />
+                          </>
+                        ) : (
+                          <p dangerouslySetInnerHTML={{ __html: dataQues.question }} />
+                        )}
                       </div>
 
                       {dataQues.choices.map((choice, choiceIndex) => (
                         <div key={`$dataQues_${quesIndex}_choices_${choiceIndex}`}>
                           <div className='flex items-center mt-3'>
-                            <div className='md:w-[8%] xs:w-[10%]'>Lựa chọn {choiceIndex + 1}: </div>{' '}
-                            <p dangerouslySetInnerHTML={{__html: choice}}/>
-                            {/* <InputEditor value={choice} handleChange={(e) => handleAnswerChange(dataQues.id, e)}/> */}
+                            <div
+                              className={`md:w-[6%] xs:w-[12%] ${
+                                dataQues.answer === choice && 'font-bold text-green-500'
+                              }`}
+                            >
+                              Lựa chọn {choiceIndex + 1}:{' '}
+                            </div>{' '}
+                            {listEditQuestion.find((item) => item === dataQues.id) ? (
+                              <>
+                                <Tooltip title={!choice ? 'Nhập nội dung đáp án trước' : undefined}>
+                                  <Radio
+                                    checked={choice && dataQues.answer === choice}
+                                    name='choice'
+                                    onChange={(e) => handleAnswerChange(dataQues.id, e.target.value)}
+                                    value={choice}
+                                  />
+                                </Tooltip>
+                                <InputEditor
+                                  placeholder='Chưa có đáp án'
+                                  value={choice}
+                                  handleChange={(e) => handleChoiceChange(dataQues.id, choiceIndex, e)}
+                                />
+                              </>
+                            ) : (
+                              <p dangerouslySetInnerHTML={{ __html: choice }} />
+                            )}
                           </div>
                         </div>
                       ))}
                     </CardContent>
-                    {/* <CardContent>
-                      <div className='flex items-center mb-3'>
-                        <div className='md:w-[8%] xs:w-[10%] font-bold'>CÂU {quesIndex + 1}: </div>
-
-                        <InputEditor value={dataQues.question} />
-                      </div>
-                      <div className='flex items-center'>
-                        <div className='md:w-[8%] xs:w-[10%]  font-bold'>Đáp án: </div>{' '}
-                        <InputEditor value={dataQues.answer} />
-                      </div>
-
-                      {dataQues.choices.map((choice, choiceIndex) => (
-                        <div key={`$dataQues_${quesIndex}_choices_${choiceIndex}`}>
-                          <div className='flex items-center mt-3'>
-                            <div className='md:w-[8%] xs:w-[10%]'>Lựa chọn {choiceIndex + 1}: </div>{' '}
-                            <InputEditor value={choice} />
-                          </div>
-                        </div>
-                      ))}
-                    </CardContent> */}
                   </Card>
                 ))
               )}
-              {/* <Button onClick={handleAddQuestion} className='!mt-3' variant='contained'>
-                Thêm câu hỏi
-              </Button> */}
             </>
           )}
           {!isFetching && selectedCategoryID && (
             <div className='fixed top-[1.5%] left-[50%] z-[1000] flex items-center justify-center'>
-              <Button variant='contained' color='success'>
-                Hoàn thành
+              <Button onClick={handleSubmit} variant='contained' color='success'>
+                <SaveOutlinedIcon className='mr-2' /> Lưu
               </Button>
             </div>
           )}
